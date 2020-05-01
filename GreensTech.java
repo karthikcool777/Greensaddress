7d4459acbba608f5fce75d6ddeca7929024cba1a
karthikcool777 <64627703+karthikcool777@users.noreply.github.com>
2020-05-01 15:25:28 +0530

    Add files via upload

---
+++ GreensTech.java
@@ -0,0 +1,48 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensOmr() {
+	System.out.println("Greens technology Omr : Plot No.19, Balamurugan Garden, OMR Road, Okkiam, "
+			+ "\n Thoraipakkam Landmark: ICICI Bank Upstairs, opposite "
+			+ "to Sankeetha hotel, Tamil Nadu 600097");
+
+	}
+	private void greensAdayar() {
+		// TODO Auto-generated method stub
+System.out.println("Greens technologu Adayar : 11, 1st St, Padmanabha Nagar, Adyar,"
+		+ "Chennai, Tamil Nadu 600020");
+	}
+	
+	private void greensTambaram() {
+		// TODO Auto-generated method stub
+System.out.println("Greens technology tambaram :No.1, AppaRao colony, Tambaram Sanatorium "
+		+ " \n Lankmark: Opposite of Sri Rama Anjaneyar Temple,"
+		+ "near HP Petrol Bulk, Chennai, Tamil Nadu 600047");
+	}
+	private void greensvelacherry() {
+		// TODO Auto-generated method stub
+System.out.println(" Greens technology velacherry : 28, Nagendra Nagar, "
+		+ "opposite Phoenix Market City, Velachery, "
+		+ "Chennai, Tamil Nadu 600042");
+	}
+	private void greensAnnanagar() {
+		// TODO Auto-generated method stub
+System.out.println(" Greens Annanagar :  4th floor, SDV Arcade, AB-5, 2nd Ave, AB Blo"
+		+ "ck, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040");
+	}
+public static void main(String[] args) {
+	
+	GreensTech g = new GreensTech();
+    g.greensOmr();
+    g.greensAdayar();
+    g.greensTambaram();
+    g.greensvelacherry();
+    g.greensAnnanagar();
+}
+
+
+
+}
+
+
